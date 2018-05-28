78d241794ef9718c119cb5dbd285c9fcd1fd35af
Abutty <aboobacker5577@gmail.com>
2018-05-28 13:52:20 +0530

    first post method req <name> res <list name>

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,22 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/',function (req,res) {
+  console.log(req.body.name);
 
+    if (req.body.name == "ebin") {
+        res.json({
+            status: 200,
+            message: {
+                name: ['Abu','sonu','nikil']
+            }
+        })
+    } else {
+        res.json({
+            status: 404,
+            message: 'data Not fond'
+        })
+
+    }
+})
 module.exports = router;
